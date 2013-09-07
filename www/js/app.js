7d9d6367a699f3f8b5435d45497bf0a0bf504544
Will Crichton <willcrichton@cmu.edu>
2013-09-06 21:28:53 -0400

    Changed file structure

---
+++ www/js/app.js
@@ -0,0 +1,30 @@
+define(function(require){
+    'use strict';
+
+    requirejs.config({
+        enforceDefine: true,
+        inlineText: true,
+        urlArgs: "bust=" + (new Date()).getTime(),
+
+        shim: {
+            'jQuery': {
+                exports: '$'
+            },
+
+            'Underscore': {
+                exports: '_'
+            },
+
+            'Backbone': {
+                deps: ['Underscore', 'jQuery'],
+                exports: 'Backbone'
+            }
+        },
+
+        paths: {
+            'jQuery'     : 'vendor/jquery-2.0.3.min',
+            'Underscore' : 'vendor/underscore-min',
+            'Backbone'   : 'vendor/backbone-min'
+        }
+    });
+});
